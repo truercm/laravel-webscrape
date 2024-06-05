2b422e732abb17c42169e36e4a42c03eb8b6caa6
Purnendu Chandan <purnendu@extreme-agile.com>
2024-06-05 19:23:57 +0530

    Parse specialities page data with error handling

--- src/Jobs/ParseSpecialitiesPage.php
+++ src/Jobs/ParseSpecialitiesPage.php
@@ -7,8 +7,12 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 use Symfony\Component\DomCrawler\Crawler;
+use TrueRcm\LaravelWebscrape\Actions\UpdateCrawlResult;
 use TrueRcm\LaravelWebscrape\Enums\CrawlResultStatus;
+use TrueRcm\LaravelWebscrape\Exceptions\CrawlException;
 use TrueRcm\LaravelWebscrape\Models\CrawlResult;
 
 class ParseSpecialitiesPage implements ShouldQueue
@@ -17,6 +21,11 @@ class ParseSpecialitiesPage implements ShouldQueue
     use InteractsWithQueue;
     use Queueable;
     use SerializesModels;
+
+    protected array $values = [];
+    protected CrawlResultStatus $process_status = CrawlResultStatus::COMPLETED;
+    protected Crawler $crawler;
+    protected ?string $error = null;
 
     public function __construct(
         protected CrawlResult $crawlResult
@@ -31,110 +40,24 @@ class ParseSpecialitiesPage implements ShouldQueue
 
     public function handle()
     {
-        $this->crawlResult->forceFill([
-            'process_status' => CrawlResultStatus::COMPLETED,
-        ]);
-        $result = [];
-        $crawler = new Crawler($this->crawlResult->body, $this->crawlResult->url);
+        $this->crawler = new Crawler($this->crawlResult->body, $this->crawlResult->url);
 
         try {
-            $specialties = [];
-            $primarySpecialty = [];
-            $primarySpecialtySection = $crawler->filter('div#PrimarySpecialityPlaceHolder');
+            $specialty = collect([]);
 
-            $primarySpecialty['is_primary'] = true;
-            $input = $primarySpecialtySection->filterXPath('//select[contains(@name, ".SpecialtyNameId")]/option[contains(@selected, "selected")]');
-            $primarySpecialty['nucc_code'] = $input->count() ? $input->attr('value') : null;
-            $primarySpecialty['label'] = $input->count() ? $input->text() : null;
-            $primarySpecialty['certification_status'] = true;
+            throw_if(
+                0 == $this->primarySection()->count() /* is not good */,
+                CrawlException::parsingFailed($this->crawlResult)
+            );
 
-            $address = [];
-            $address['address_type'] = 'office';
-            $input = $primarySpecialtySection->filterXPath('//select[contains(@name, ".CountryId")]/option[contains(@selected, "selected")]');
-            $address['country'] = $input->count() ? $input->text() : null;
+            $specialty->push($this->handleSpecialty($this->primarySection()));
+            $specialty->push($this->handleSecondarySpecialty($this->secondarySection()));
 
-            $input = $primarySpecialtySection->filterXPath('//select[contains(@name, ".StateId")]/option[contains(@selected, "selected")]');
-            $address['state'] = $input->count() ? $input->text() : null;
+            $this->values['specialties'] = $specialty->filter()->toArray();
 
-            $address['line_1'] = $primarySpecialtySection->filterXPath('//input[contains(@name, ".Street1")]')->attr('value');
-            $address['line_2'] = $primarySpecialtySection->filterXPath('//input[contains(@name, ".Street2")]')->attr('value');
-            $address['city'] = $primarySpecialtySection->filterXPath('//input[contains(@name, ".City")]')->attr('value');
-            $address['province'] = $primarySpecialtySection->filterXPath('//input[contains(@name, ".Province")]')->attr('value');
-            $address['zip'] = $primarySpecialtySection->filterXPath('//input[contains(@name, ".ZipCode")]')->attr('value');
-            $address['zip_extension'] = null;
-
-            $primarySpecialty['address'] = $address;
-
-            $primarySpecialty['Percent of Practice'] = $crawler->filterXPath('//input[contains(@name, ".PercentOfPractice")]')->attr('value');
-            $inputs = $primarySpecialtySection->filter('div#PrimaryBoardCertifiedDiv label.radio');
-            $boardCertified = [];
-            $inputs->each(function ($node, $i) use (&$boardCertified) {
-                $radio = $node->filterXPath('//input[@type="radio"]')
-                    ->filterXPath('//input[@checked="checked"]');
-                $boardCertified[$node->text()] = $radio->count() ? true : false;
-            });
-
-            $primarySpecialty['boardCertified'] = $boardCertified;
-
-            $input = $primarySpecialtySection->filterXPath('//select[contains(@name, ".SpecialtyBoardId")]/option[contains(@selected, "selected")]');
-            $primarySpecialty['certification_board'] = $input->count() ? $input->text() : null;
-
-            $primarySpecialty['certification_number'] = $primarySpecialtySection->filterXPath('//input[contains(@name, ".CertificationNumber")]')->attr('value');
-            $primarySpecialty['certification_created_at'] = $primarySpecialtySection->filterXPath('//input[contains(@name, ".CertificationDate")]')->attr('value');
-
-            $boardCertificationHaveExpirationDate = [];
-            $inputs = $primarySpecialtySection->filterXPath('//label[contains(@for, "DoesYourBoardCertificationExpirationDate")]')
-                ->siblings()
-                ->filter('label.radio');
-
-            $inputs->each(function ($node, $i) use (&$boardCertificationHaveExpirationDate) {
-                $radio = $node->filterXPath('//input[@type="radio"]')
-                    ->filterXPath('//input[@checked="checked"]');
-                $boardCertificationHaveExpirationDate[$node->text()] = $radio->count() ? true : false;
-            });
-
-            $primarySpecialty['certification_expires'] = $boardCertificationHaveExpirationDate['Yes'];
-
-            $primarySpecialty['certification_expires_at'] = $primarySpecialtySection->filterXPath('//input[contains(@name, ".ExpirationDate")]')->attr('value');
-            $primarySpecialty['certification_updated_at'] = $primarySpecialtySection->filterXPath('//input[contains(@name, ".RecertificationDate")]')->attr('value');
-
-            $planningToCertificationOrReCertification = [];
-            $inputs = $primarySpecialtySection->filterXPath('//label[contains(@for, "PlanToPursueBoardCertification")]')
-                ->siblings()
-                ->filter('label.radio');
-
-            $inputs->each(function ($node, $i) use (&$planningToCertificationOrReCertification) {
-                $radio = $node->filterXPath('//input[@type="radio"]')
-                    ->filterXPath('//input[@checked="checked"]');
-                $planningToCertificationOrReCertification[$node->text()] = $radio->count() ? true : false;
-            });
-
-            $primarySpecialty['plans_to_update'] = $planningToCertificationOrReCertification['Yes'];
-
-            $specialties[] = $primarySpecialty;
-
-            $result['specialties'] = $specialties;
-
-            $secondarySpecialty = [];
-            $secondarySpecialtySection = $crawler->filter('div#SecondarySpecialitySection');
-
-            $haveSecondarySpecialty = [];
-            $inputs = $secondarySpecialtySection->filterXPath('//label[contains(@for, "DoYouHaveASecondarySpecialty")]')
-                ->siblings()
-                ->filter('label.radio');
-
-            $inputs->each(function ($node, $i) use (&$haveSecondarySpecialty) {
-                $radio = $node->filterXPath('//input[@type="radio"]')
-                    ->filterXPath('//input[@checked="checked"]');
-                $haveSecondarySpecialty[$node->text()] = $radio->count() ? true : false;
-            });
-
-            $secondarySpecialty['Do you have a Secondary Specialty?'] = $haveSecondarySpecialty;
-
-            $result['secondarySpecialty'] = $secondarySpecialty;
 
             $haveCertifications = [];
-            $inputs = $crawler->filterXPath('//label[contains(@for, "CertificationsVM_LifeSupportCertification")]')
+            $inputs = $this->crawler->filterXPath('//label[contains(@for, "CertificationsVM_LifeSupportCertification")]')
                 ->siblings()
                 ->filter('label.radio');
 
@@ -144,74 +67,175 @@ class ParseSpecialitiesPage implements ShouldQueue
                 $haveCertifications[$node->text()] = $radio->count() ? true : false;
             });
 
-            $result['Do you have Certifications?'] = $haveCertifications;
+            $this->values['Do you have Certifications?'] = $haveCertifications;
 
-            $result['Other Interests'] = $crawler->filter('textarea[name="AreasOfProfessionalPracticeInterest"]')->text();
+            $this->values['Other Interests'] = $this->crawler->filter('textarea[name="AreasOfProfessionalPracticeInterest"]')->text();
 
-            $specialExperienceSkillsSection = $crawler->filter('div#SpecialExperienceSkillsAndTrainingPlaceHolder');
+            $skills = collect([]);
 
-            $patientPopulations = [];
-            $inputs = $specialExperienceSkillsSection->filterXPath('//label[contains(@for, "Patient_populations")]')
-                ->siblings()
-                ->filter('div.row')
-                ->filter('div.checkbox-style-dev');
-
-            $inputs->each(function ($node, $i) use (&$patientPopulations) {
-                $checkbox = $node->filterXPath('//input[@type="checkbox"]')
-                    ->filterXPath('//input[@checked="checked"]');
-                $patientPopulations[$node->text()] = $checkbox->count() ? true : false;
+            $this->skillsNodes()->each(function (Crawler $node, $i) use ($skills) {
+                $this->handleSkillsNode($node, $skills);
             });
-            $result['patient_populations'] = $patientPopulations;
+            $this->values['skills'] = $skills->toArray();
 
-            $physicalConditions = [];
-            $inputs = $specialExperienceSkillsSection->filterXPath('//label[contains(@for, "Physical_Conditions")]')
-                ->siblings()
-                ->filter('div.row')
-                ->filter('div.checkbox-style-dev');
-
-            $inputs->each(function ($node, $i) use (&$physicalConditions) {
-                $checkbox = $node->filterXPath('//input[@type="checkbox"]')
-                    ->filterXPath('//input[@checked="checked"]');
-                $physicalConditions[$node->text()] = $checkbox->count() ? true : false;
-            });
-            $result['physical_conditions'] = $physicalConditions;
-
-            $areaOfExpertise = [];
-            $inputs = $specialExperienceSkillsSection->filterXPath('//label[contains(@for, "Area_of_Expertise")]')
-                ->siblings()
-                ->filter('div.row')
-                ->filter('div.checkbox-style-dev');
-
-            $inputs->each(function ($node, $i) use (&$areaOfExpertise) {
-                $checkbox = $node->filterXPath('//input[@type="checkbox"]')
-                    ->filterXPath('//input[@checked="checked"]');
-                $areaOfExpertise[$node->text()] = $checkbox->count() ? true : false;
-            });
-            $result['areas_of_expertise'] = $areaOfExpertise;
-
-            $treatmentOptions = [];
-            $inputs = $specialExperienceSkillsSection->filterXPath('//label[contains(@for, "Treatment_Options")]')
-                ->siblings()
-                ->filter('div.row')
-                ->filter('div.checkbox-style-dev');
-
-            $inputs->each(function ($node, $i) use (&$treatmentOptions) {
-                $checkbox = $node->filterXPath('//input[@type="checkbox"]')
-                    ->filterXPath('//input[@checked="checked"]');
-                $treatmentOptions[$node->text()] = $checkbox->count() ? true : false;
-            });
-            $result['treatment_options'] = $treatmentOptions;
         } catch (\Exception $e) {
-            $error = __('Error :message at line :line', ['message' => $e->getMessage(), 'line' => $e->getLine()]);
-            $result['error'] = $error;
-            $this->crawlResult->forceFill([
-                'process_status' => CrawlResultStatus::ERROR,
-            ]);
+            $this->error = $e->getMessage();
+            $this->process_status = CrawlResultStatus::ERROR;
         }
 
-        $this->crawlResult->forceFill([
+        if ($this->error) {
+            $this->values['error'] = $this->error;
+        }
+
+        resolve(UpdateCrawlResult::class)->run($this->crawlResult, $this->toArray());
+    }
+
+    protected function toArray(): array
+    {
+        return [
             'processed_at' => now(),
-            'result' => $result,
-        ])->save();
+            'result' => $this->values,
+            'process_status' => $this->process_status->value,
+        ];
+    }
+
+    protected function primarySection(): Crawler
+    {
+        return $this->crawler->filter('div#PrimarySpecialityPlaceHolder');
+    }
+
+    protected function secondarySection(): Crawler
+    {
+        return $this->crawler->filter('div#SecondarySpecialitySection');
+    }
+
+    protected function skillsNodes(): Crawler
+    {
+        return $this->crawler->filter('div#SpecialExperienceSkillsAndTrainingPlaceHolder div.col-xs-6');
+    }
+
+    protected function handleSpecialty($specialtySection): array
+    {
+        $specialty = [];
+
+        $specialty['is_primary'] = $specialtySection->attr('id') == 'PrimarySpecialityPlaceHolder';
+        $input = $specialtySection->filterXPath('//select[contains(@name, ".SpecialtyNameId")]/option[contains(@selected, "selected")]');
+        $specialty['nucc_code'] = $input->count() ? $input->attr('value') : null;
+        $specialty['label'] = $input->count() ? $input->text() : null;
+        $specialty['percent_of_practice'] = $specialtySection->filterXPath('//input[contains(@name, ".PercentOfPractice")]')->attr('value');
+
+        $inputs = $specialtySection->filterXPath('//label[contains(@for, "BoardCertifid")]')
+            ->siblings()
+            ->filter('label.radio');
+        $boardCertified = [];
+        $inputs->each(function ($node, $i) use (&$boardCertified) {
+            $radio = $node->filterXPath('//input[@type="radio"]')
+                ->filterXPath('//input[@checked="checked"]');
+            $boardCertified[$node->text()] = $radio->count() ? true : false;
+        });
+
+        $specialty['certification_status'] = $boardCertified['Yes'];
+
+        if($boardCertified['Yes']){
+            $address = [];
+            $address['address_type'] = 'office';
+            $input = $specialtySection->filterXPath('//select[contains(@name, ".CountryId")]/option[contains(@selected, "selected")]');
+            $address['country'] = $input->count() ? $input->text() : null;
+
+            $input = $specialtySection->filterXPath('//select[contains(@name, ".StateId")]/option[contains(@selected, "selected")]');
+            $address['state'] = $input->count() ? $input->text() : null;
+
+            $address['line_1'] = $specialtySection->filterXPath('//input[contains(@name, ".Street1")]')->attr('value');
+            $address['line_2'] = $specialtySection->filterXPath('//input[contains(@name, ".Street2")]')->attr('value');
+            $address['city'] = $specialtySection->filterXPath('//input[contains(@name, ".City")]')->attr('value');
+            $address['province'] = $specialtySection->filterXPath('//input[contains(@name, ".Province")]')->attr('value');
+            $address['zip'] = $specialtySection->filterXPath('//input[contains(@name, ".ZipCode")]')->attr('value');
+            $address['zip_extension'] = null;
+
+            $specialty['address'] = $address;
+
+            $input = $specialtySection->filterXPath('//select[contains(@name, ".SpecialtyBoardId")]/option[contains(@selected, "selected")]');
+            $specialty['certification_board'] = $input->count() ? $input->text() : null;
+
+            $specialty['certification_number'] = $specialtySection->filterXPath('//input[contains(@name, ".CertificationNumber")]')->attr('value');
+            $specialty['certification_created_at'] = $specialtySection->filterXPath('//input[contains(@name, ".CertificationDate")]')->attr('value');
+
+            $boardCertificationHaveExpirationDate = [];
+            $inputs = $specialtySection->filterXPath('//label[contains(@for, "DoesYourBoardCertificationExpirationDate")]')
+                ->siblings()
+                ->filter('label.radio');
+
+            $inputs->each(function ($node, $i) use (&$boardCertificationHaveExpirationDate) {
+                $radio = $node->filterXPath('//input[@type="radio"]')
+                    ->filterXPath('//input[@checked="checked"]');
+                $boardCertificationHaveExpirationDate[$node->text()] = $radio->count() ? true : false;
+            });
+
+            $specialty['certification_expires'] = $boardCertificationHaveExpirationDate['Yes'];
+
+            $specialty['certification_expires_at'] = $specialtySection->filterXPath('//input[contains(@name, ".ExpirationDate")]')->attr('value');
+            $specialty['certification_updated_at'] = $specialtySection->filterXPath('//input[contains(@name, ".RecertificationDate")]')->attr('value');
+
+            $planningToCertificationOrReCertification = [];
+
+            $planToPursueBoardCertification = $specialtySection->filterXPath('//label[contains(@for, "PlanToPursueBoardCertification")]');
+            if($planToPursueBoardCertification->count()){
+                $inputs = $specialtySection->filterXPath('//label[contains(@for, "PlanToPursueBoardCertification")]')
+                    ->siblings()
+                    ->filter('label.radio');
+
+                $inputs->each(function ($node, $i) use (&$planningToCertificationOrReCertification) {
+                    $radio = $node->filterXPath('//input[@type="radio"]')
+                        ->filterXPath('//input[@checked="checked"]');
+                    $planningToCertificationOrReCertification[$node->text()] = $radio->count() ? true : false;
+                });
+
+                $specialty['plans_to_update'] = $planningToCertificationOrReCertification['Yes'];
+            }
+        }
+
+
+        return $specialty;
+    }
+
+    protected function handleSecondarySpecialty($secondarySpecialtySection): ?array
+    {
+        $specialty = null;
+        $haveSecondarySpecialty = [];
+        $inputs = $secondarySpecialtySection->filterXPath('//label[contains(@for, "DoYouHaveASecondarySpecialty")]')
+            ->siblings()
+            ->filter('label.radio');
+
+        $inputs->each(function ($node, $i) use (&$haveSecondarySpecialty) {
+            $radio = $node->filterXPath('//input[@type="radio"]')
+                ->filterXPath('//input[@checked="checked"]');
+            $haveSecondarySpecialty[$node->text()] = $radio->count() ? true : false;
+        });
+
+        if($haveSecondarySpecialty['Yes']){
+            $specialty = $this->handleSpecialty($secondarySpecialtySection);
+        }
+
+        return $specialty;
+    }
+
+    protected function handleSkillsNode($skillsNode, $skills): Collection
+    {
+        $values = [];
+        $label = $skillsNode->filter('label')->first();
+        $text = Str::of($label->text())->snake()->__toString();
+
+        $inputs = $label
+            ->siblings()
+            ->filter('div.row')
+            ->filter('div.checkbox-style-dev');
+
+        $inputs->each(function ($node, $i) use (&$values) {
+            $checkbox = $node->filterXPath('//input[@type="checkbox"]')
+                ->filterXPath('//input[@checked="checked"]');
+            $values[$node->text()] = $checkbox->count() ? true : false;
+        });
+
+        return $skills->put($text, $values);
     }
 }
---
+++ tests/Feature/Jobs/ParseSpecialitiesPageTest.php
@@ -0,0 +1,207 @@
+<?php
+
+use Illuminate\Support\Carbon;
+use Mockery\MockInterface;
+use TrueRcm\LaravelWebscrape\Actions\UpdateCrawlResult;
+use TrueRcm\LaravelWebscrape\Enums\CrawlResultStatus;
+use TrueRcm\LaravelWebscrape\Jobs\ParseSpecialitiesPage;
+use TrueRcm\LaravelWebscrape\Models\CrawlResult;
+
+beforeEach(function () {
+    Carbon::setTestNow('May 17, 2023 2:13 PM');
+});
+
+it('will parse the page', function () {
+    $html = <<<HTML
+<html>
+<body>
+<div id="PrimarySpecialityPlaceHolder">
+<select name=".SpecialtyNameId">
+<option value="123" >Nurse</option>
+<option value="456" selected="selected">Physician</option>
+</select>
+<input name=".PercentOfPractice" value="10">
+<label for="BoardCertifid">Board Certified?</label>
+<label class="radio">
+<input type="radio" >No
+</label>
+<label class="radio">
+<input type="radio" checked="checked">Yes
+</label>
+<select name=".SpecialtyBoardId">
+<option value="234" >Foo</option>
+<option value="567" selected="selected">Baa</option>
+</select>
+<select name=".StateId">
+<option value="" >Delhi</option>
+<option value="" selected="selected">UP</option>
+</select>
+<select name=".CountryId">
+<option value="" >US</option>
+<option value="" selected="selected">India</option>
+</select>
+<input name=".Street1" value="Line 1">
+<input name=".Street2" value="Line 2">
+<input name=".City" value="Noida">
+<input name=".ZipCode" value="201012">
+<input name=".Province" value="USA">
+<input name=".CertificationNumber" value="1147666">
+<input name=".CertificationDate" value="12/11/2021">
+<label for="DoesYourBoardCertificationExpirationDate">Does your board certification have an expiration date?</label>
+<label class="radio">
+<input type="radio" >No
+</label>
+<label class="radio">
+<input type="radio" checked="checked">Yes
+</label>
+<input name=".ExpirationDate" value="12/11/2023">
+<input name=".RecertificationDate" value="11/12/2024">
+<label for="PlanToPursueBoardCertification">I am planning to pursue Board Certification or Re-Certification:</label>
+<label class="radio">
+<input type="radio" >No
+</label>
+<label class="radio">
+<input type="radio" checked="checked">Yes
+</label>
+</div>
+<div id="SecondarySpecialitySection">
+<label for="DoYouHaveASecondarySpecialty">Do you have a Secondary Specialty?</label>
+<label class="radio">
+<input type="radio" checked="checked">No
+</label>
+<label class="radio">
+<input type="radio" >Yes
+</label>
+</div>
+
+<label for="CertificationsVM_LifeSupportCertification">Do you have Certifications?</label>
+<label class="radio">
+<input type="radio" checked="checked">No
+</label>
+<label class="radio">
+<input type="radio" >Yes
+</label>
+<textarea name="AreasOfProfessionalPracticeInterest">My interest</textarea>
+
+<div id="SpecialExperienceSkillsAndTrainingPlaceHolder">
+<div class="row">
+<div class="col-xs-6">
+<label>Patient populations</label>
+<div class="row">
+<div class="checkbox-style-dev">
+<input type="checkbox" checked="checked">Adolescents
+</div>
+<div class="checkbox-style-dev">
+<input type="checkbox">Children
+</div>
+</div>
+</div>
+<div class="col-xs-6">
+<label>Physical Conditions</label>
+<div class="row">
+<div class="checkbox-style-dev">
+<input type="checkbox" checked="checked">People with Disabilities
+</div>
+<div class="checkbox-style-dev">
+<input type="checkbox">Physical Disabilities
+</div>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
+HTML;
+
+    $crawlResult = CrawlResult::factory()->create(['body' => $html]);
+
+    $storeCrawlResultMock = $this->mock(UpdateCrawlResult::class, function (MockInterface $mock) use ($crawlResult) {
+        $mock->shouldReceive('run')
+            ->once()
+            ->with($crawlResult, [
+                'processed_at' => now(),
+                'process_status' => CrawlResultStatus::COMPLETED->value,
+                'result' => [
+                    "specialties" => [
+                        [
+                            "is_primary" => true,
+                            "nucc_code" => "456",
+                            "label" => "Physician",
+                            "percent_of_practice" => "10",
+                            "certification_status" => true,
+                            "address" => [
+                                "address_type" => "office",
+                                "country" => "India",
+                                "state" => "UP",
+                                "line_1" => "Line 1",
+                                "line_2" => "Line 2",
+                                "city" => "Noida",
+                                "province" => "USA",
+                                "zip" => "201012",
+                                "zip_extension" => null,
+                            ],
+                            "certification_board" => "Baa",
+                            "certification_number" => "1147666",
+                            "certification_created_at" => "12/11/2021",
+                            "certification_expires" => true,
+                            "certification_expires_at" => "12/11/2023",
+                            "certification_updated_at" => "11/12/2024",
+                            "plans_to_update" => true,
+                        ],
+                    ],
+                    "Do you have Certifications?" => [
+                        "No" => true,
+                        "Yes" => false,
+                    ],
+                    "Other Interests" => "My interest",
+                    "skills" => [
+                        "patient_populations" => [
+                            "Adolescents" => true,
+                            "Children" => false,
+                        ],
+                        "physical_conditions" => [
+                            "People with Disabilities" => true,
+                            "Physical Disabilities" => false,
+                        ],
+                    ],
+                ],
+            ])
+            ->andReturn($crawlResult);
+    });
+
+    $this->instance(UpdateCrawlResult::class, $storeCrawlResultMock);
+
+    $job = new ParseSpecialitiesPage($crawlResult);
+
+    $job->handle();
+});
+
+it('will generate error if node not found', function () {
+    $html = <<<HTML
+<html>
+<body>
+<div id="abc">
+</div>
+</body>
+</html>
+HTML;
+
+    $crawlResult = CrawlResult::factory()->create(['body' => $html, 'url' => 'http:://some.site']);
+
+    $storeCrawlResultMock = $this->mock(UpdateCrawlResult::class, function (MockInterface $mock) use ($crawlResult) {
+        $mock->shouldReceive('run')
+            ->once()
+            ->with($crawlResult, [
+                'processed_at' => now(),
+                'process_status' => CrawlResultStatus::ERROR->value,
+                'result' => ['error' => 'Parsing failed for the page with url http:://some.site'],
+            ])
+            ->andReturn($crawlResult);
+    });
+
+    $this->instance(UpdateCrawlResult::class, $storeCrawlResultMock);
+
+    $job = new ParseSpecialitiesPage($crawlResult);
+
+    $job->handle();
+});
