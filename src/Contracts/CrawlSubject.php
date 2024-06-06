2d801924bb4a33075018b6ca629803f0228fccf4
Cyrill N Kalita <ckalita@flssol.com>
2024-06-06 12:55:44 -0500

    Add property 'credentials' to CrawlSubject interface

--- src/Contracts/CrawlSubject.php
+++ src/Contracts/CrawlSubject.php
@@ -4,6 +4,7 @@ namespace TrueRcm\LaravelWebscrape\Contracts;
 
 /**
  * @mixin \Illuminate\Database\Eloquent\Model
+ * @property array $credentials
  */
 interface CrawlSubject
 {
--- src/CrawlTraveller.php
+++ src/CrawlTraveller.php
@@ -5,40 +5,52 @@ namespace TrueRcm\LaravelWebscrape;
 use Illuminate\Support\Collection;
 use TrueRcm\LaravelWebscrape\Contracts\BrowserClient;
 use TrueRcm\LaravelWebscrape\Contracts\CrawlResult;
-use TrueRcm\LaravelWebscrape\Models\CrawlSubject;
-use TrueRcm\LaravelWebscrape\Models\CrawlTargetUrl;
+use TrueRcm\LaravelWebscrape\Contracts\CrawlSubject;
+use TrueRcm\LaravelWebscrape\Contracts\CrawlTargetUrl;
 
 class CrawlTraveller
 {
     protected CrawlSubject $subject;
     protected ?BrowserClient $browser = null;
 
+    /** @var \TrueRcm\LaravelWebscrape\Contracts\CrawlResult[] */
     protected array $crawledPages = [];
 
+    /**
+     * Create new CrawlTraveller.
+     */
     public function __construct(CrawlSubject $subject)
     {
         $this->subject = $subject;
     }
 
     /**
-     * @param \TrueRcm\LaravelWebscrape\Contracts\CrawlSubject $subject
-     * @return $this
+     * Statically create a new CralwTraveller.
      */
-    public static function make(CrawlSubject $subject): self
+    public static function make(CrawlSubject $subject): static
     {
         return new static($subject);
     }
 
+    /**
+     * Get the subject of the CrawlTraveller.
+     */
     public function subject(): CrawlSubject
     {
         return $this->subject;
     }
 
+    /**
+     * Get the BrowserClient.
+     */
     public function getBrowser(): BrowserClient
     {
         return $this->browser;
     }
 
+    /**
+     * Set the BrowserClient.
+     */
     public function setBrowser(BrowserClient $bowser): self
     {
         $this->browser = $bowser;
@@ -46,20 +58,31 @@ class CrawlTraveller
         return $this;
     }
 
+    /**
+     * Collect targets to crawl.
+     */
     public function targets(): Collection
     {
         return $this->subject
-           ->targetUrls
+            ->loadMissing('targetUrls')
+            ->getRelation('targetUrls')
             ->map(fn (CrawlTargetUrl $crawlTargetUrl) => $crawlTargetUrl->setAttribute('url', $crawlTargetUrl->url_template));
     }
 
+    /**
+     * Get authentication URL.
+     */
     public function authUrl(): ?string
     {
         return $this->subject
-            ->crawlTarget
+            ->loadMissing('crawlTarget')
+            ->getRelation('crawlTarget')
             ->auth_url ?? null;
     }
 
+    /*
+     * Resolve the identifier for the login button
+     */
     public function authButtonIdentifier(): ?string
     {
         return $this->subject
@@ -67,11 +90,17 @@ class CrawlTraveller
             ->auth_button_text ?? null;
     }
 
+    /**
+     * get crawling credentials.
+     */
     public function getCrawlingCredentials(): array
     {
         return $this->subject->credentials;
     }
 
+    /**
+     * Add anotehr crawl result.
+     */
     public function addCrawledPage(CrawlResult $page): self
     {
         $this->crawledPages[] = $page;
@@ -79,6 +108,9 @@ class CrawlTraveller
         return $this;
     }
 
+    /**
+     * Get crawled pages.
+     */
     public function getCrawledPages(): Collection
     {
         return collect($this->crawledPages);
