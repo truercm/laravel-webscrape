9b4d1e51b23383dc37cc7511d74be25b32f684cf
Purnendu Chandan <purnendu@extreme-agile.com>
2024-06-06 19:51:44 +0530

    Refactor webscrape browser facade resolution test

--- tests/Feature/Facades/WebscrapeBrowserTest.php
+++ tests/Feature/Facades/WebscrapeBrowserTest.php
@@ -5,8 +5,6 @@ use TrueRcm\LaravelWebscrape\Facades\WebscrapeBrowser;
 
 it('will handle webscrape browser facade resolution', function () {
     $browser = $this->mock(BrowserClient::class);
-    $this->assertEquals(BrowserClient::class, WebscrapeBrowser::getFacadeAccessor());
     $this->assertInstanceOf(BrowserClient::class, WebscrapeBrowser::getFacadeRoot());
-    $this->assertInstanceOf(BrowserClient::class, app(BrowserClient::class));
-    $this->assertSame($browser, app(BrowserClient::class));
+    $this->assertSame($browser, WebscrapeBrowser::getFacadeRoot());
 });
