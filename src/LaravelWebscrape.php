c3ec3c478b00e3c01e33db7676fa16f52bbf855b
Cyrill N Kalita <ckalita@flssol.com>
2024-06-12 10:18:02 -0500

    Remove deprecated webscrape facades and tests

--- src/LaravelWebscrape.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace TrueRcm\LaravelWebscrape;
-
-class LaravelWebscrape
-{
-}
--- tests/Feature/Facades/WebscrapeBrowserTest.php
+++ tests/Feature/Facades/WebscrapeBrowserTest.php
@@ -1,15 +1,17 @@
 <?php
 
-use Symfony\Component\Panther\Client;
+use TrueRcm\LaravelWebscrape\Browser;
 use TrueRcm\LaravelWebscrape\Contracts\BrowserClient;
 use TrueRcm\LaravelWebscrape\Facades\WebscrapeBrowser;
 
 it('will handle webscrape browser facade resolution when mocked', function () {
     $browser = $this->mock(BrowserClient::class);
+
     $this->assertInstanceOf(BrowserClient::class, WebscrapeBrowser::getFacadeRoot());
     $this->assertSame($browser, WebscrapeBrowser::getFacadeRoot());
 });
 
 it('will handle webscrape browser facade resolution when called directly', function () {
-    $this->assertInstanceOf(Client::class, WebscrapeBrowser::getFacadeRoot());
+    $this->assertInstanceOf(BrowserClient::class, WebscrapeBrowser::getFacadeRoot());
+    $this->assertInstanceOf(Browser::class, WebscrapeBrowser::getFacadeRoot());
 });
