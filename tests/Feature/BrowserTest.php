57f89622cfcba85b9379a3fc616aa92434c8fb20
Cyrill N Kalita <ckalita@flssol.com>
2024-06-17 15:50:57 -0500

    Refactor BrowserTest.php and add bootstrap.php file

--- tests/Feature/BrowserTest.php
+++ tests/Feature/BrowserTest.php
@@ -1,20 +1,10 @@
 <?php
 
-use DG\BypassFinals;
 use Symfony\Component\Panther\Client;
-use Symfony\Component\Panther\ProcessManager\BrowserManagerInterface;
 use TrueRcm\LaravelWebscrape\Browser;
 
-beforeEach(function () {
-    BypassFinals::enable();
-});
-
 it('will forward calls to client from browser proxy', function () {
-    $manager = $this->mock(BrowserManagerInterface::class, function ($mock) {
-        $mock->shouldReceive('quit')->andReturnSelf();
-    });
-
-    $client = $this->partialMock(Client::class, fn () => new Client($manager));
+    $client = $this->mock(Client::class);
 
     $client
         ->expects('whatever')
