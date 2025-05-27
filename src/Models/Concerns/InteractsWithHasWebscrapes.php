0498c65953efc8b494868468bacd8859e7841c1f
Purnendu Chandan <purnendu@extreme-agile.com>
2025-05-27 13:51:26 +0530

    Add method to interact with has webscrapes

--- src/Models/Concerns/InteractsWithHasWebscrapes.php
+++ src/Models/Concerns/InteractsWithHasWebscrapes.php
@@ -3,6 +3,7 @@
 namespace TrueRcm\LaravelWebscrape\Models\Concerns;
 
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 trait InteractsWithHasWebscrapes
 {
@@ -12,6 +13,15 @@ trait InteractsWithHasWebscrapes
     public function crawlSubjects(): MorphMany
     {
         return $this->morphMany(config('webscrape.models.subject'), 'model');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\MorphOne
+     */
+    public function crawlSubject(): MorphOne
+    {
+        return $this->morphOne(config('webscrape.models.subject'), 'model')
+            ->latest();
     }
 
     /**
